df4b94bdf25b159b5b9dd59f9c70172297ad44d1
unknown <aliksray@gmail.com>
2021-05-26 17:44:38 +0300

    Слайдер сделан mobile-only

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
-    $('.highlights__list').slick({
+// slider
+  $slick_slider = $('.highlights__list');
+  settings_slider = {
         accessibility: true,
         arrows: false,
         autoplay: true,
         dots: true,
         appendDots: $(highlights_for_dots),
+  }
+  slick_on_mobile( $slick_slider, settings_slider);
 
+// slick on mobile
+  function slick_on_mobile(slider, settings){
+    $(window).on('load resize', function() {
+      if ($(window).width() > 767) {
+        if (slider.hasClass('slick-initialized')) {
+          slider.slick('unslick');
+        }
+        return
+      }
+      if (!slider.hasClass('slick-initialized')) {
+        return slider.slick(settings);
+      }
     });
-  });
+  };
