4472b714c490c09bc3684876f74428169d2c7cf0
unknown <aliksray@gmail.com>
2021-05-26 17:19:25 +0300

    Добавлен слайдер, исправлены ошибки верстки спикеров в м.версии

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.highlights__list').slick({
+        accessibility: true,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        appendDots: $(highlights_for_dots),
+
+    });
+  });
